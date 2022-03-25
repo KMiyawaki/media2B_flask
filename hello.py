71f77b756f1f0e635075e61737bd97a1791aedd5
K.Miyawaki <5770105+KMiyawaki@users.noreply.github.com>
2022-03-26 06:03:37 +0900

    fix: quotation

--- hello.py
+++ hello.py
@@ -9,5 +9,5 @@ def hello_world():
     return 'Hello World!'
 
 
-if __name__ == "__main__":
-    g_app.run(host="0.0.0.0", port="5000", debug=True)
+if __name__ == '__main__':
+    g_app.run(host='0.0.0.0', port=5000, debug=True)
